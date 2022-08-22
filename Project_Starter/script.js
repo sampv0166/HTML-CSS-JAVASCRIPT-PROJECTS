67ced3f7bc1515f3777d03e3534bdc626a0e42c9
abdu <sampv0166@gmail.com>
2022-08-22 23:26:09 +0400

    blur image is working

--- Project_Starter/script.js
+++ Project_Starter/script.js
@@ -11,4 +11,11 @@ function blurring() {
         clearInterval(int)
     }
     loadText.innerHTML = load
+    loadText.style.opacity = scale(load, 0,100,1,0)
+    bg.style.filter = `blur(${scale(load, 0,100,30,0)}px)`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) =>{
+    return ((num - in_min) * (out_max - out_min) / 
+    (in_max - in_min) + out_min)
 }
